ae729dc89e7dcda0be9dff87a5c7f6dff2b761b9
damcoll <damcoll7259@gmail.com>
2016-11-08 21:52:26 +0100

    1.2

--- Form1.cs
+++ Form1.cs
@@ -48,6 +48,7 @@ namespace LesColons
                     if (select == 1 && plateau.bois >= plateau.maison.bois)
                     {
                         plateau.bois -= plateau.maison.bois;
+                        plateau.population_max += 3;
                         plateau.pop += 1;
                         plateau.pop_dispo += 1;
                         plateau.tab[i] = 1;
@@ -97,6 +98,37 @@ namespace LesColons
                         tableLayoutPanel1.Controls[i].BackgroundImage = LesColons.Properties.Resources.monument;
                         tableLayoutPanel1.Controls[i].BackgroundImageLayout = ImageLayout.Zoom;
                     }
+                    if (select == 7 && plateau.or >= plateau.ecole.or && plateau.bois >= plateau.ecole.bois && plateau.fer >= plateau.ecole.fer)
+                    {
+                        plateau.or -= plateau.ecole.or;
+                        plateau.technologie += plateau.prod_ecole;
+                        plateau.bois -= plateau.ecole.bois;
+                        plateau.fer -= plateau.ecole.fer;
+                        plateau.tab[i] = 7;
+                        tableLayoutPanel1.Controls[i].BackgroundImage = LesColons.Properties.Resources.ecole;
+                        tableLayoutPanel1.Controls[i].BackgroundImageLayout = ImageLayout.Zoom;
+                    }
+                    if (select == 8 && plateau.or >= plateau.entrepos.or && plateau.bois >= plateau.entrepos.bois && plateau.fer >= plateau.entrepos.fer)
+                    {
+                        plateau.or -= plateau.entrepos.or;
+                        plateau.fer_max += plateau.entrepos.stock_fer;
+                        plateau.bois_max += plateau.entrepos.stock_bois;
+                        plateau.bois -= plateau.entrepos.bois;
+                        plateau.fer -= plateau.entrepos.fer;
+                        plateau.tab[i] = 8;
+                        tableLayoutPanel1.Controls[i].BackgroundImage = LesColons.Properties.Resources.entrepos;
+                        tableLayoutPanel1.Controls[i].BackgroundImageLayout = ImageLayout.Zoom;
+                    }
+                    if (select == 9 && plateau.or >= plateau.gardeManger.or && plateau.bois >= plateau.gardeManger.bois && plateau.fer >= plateau.gardeManger.fer)
+                    {
+                        plateau.or -= plateau.gardeManger.or;
+                        plateau.nour_max += plateau.gardeManger.stock_nour;
+                        plateau.bois -= plateau.gardeManger.bois;
+                        plateau.fer -= plateau.gardeManger.fer;
+                        plateau.tab[i] = 9;
+                        tableLayoutPanel1.Controls[i].BackgroundImage = LesColons.Properties.Resources.gardeManger;
+                        tableLayoutPanel1.Controls[i].BackgroundImageLayout = ImageLayout.Zoom;
+                    }
                 }
             }
             label1.Text = "Bois : " + Convert.ToString(plateau.bois);
@@ -121,16 +153,27 @@ namespace LesColons
             public double or;
             public double pop;
             public double pop_dispo;
+            public double prod_ecole;
             public int date;
             public double prestige;
             public double nataliter;
+            public double technologie;
+            public double bonheur;
+            public double population_max;
+            public double bois_max;
+            public double fer_max;
+            public double nour_max;
+            public double or_max;
 
+            public gardeManger gardeManger;
+            public entrepos entrepos;
             public maison maison;
             public route route;
             public ferme ferme;
             public mine mine;
             public scierie scierie;
             public monument monu;
+            public ecole ecole;
             double prod_ferme;
             double prod_mine;
             double prod_scirie;
@@ -146,14 +189,25 @@ namespace LesColons
                 prod_ferme = 1.5;
                 prod_mine = 0.5;
                 prod_scirie = 0.5;
+                prod_ecole = 0.5;
                 prestige = 0;
                 nataliter = 0.005;
+                bonheur = 0;
+                technologie = 0;
+                population_max = 0;
+                or_max = 1000;
+                fer_max = 100;
+                bois_max = 100;
+                nour_max = 100;
                 maison = new maison(10);
                 route = new route(10);
                 ferme = new ferme(10, 10, 1);
                 mine = new mine(12, 12, 1);
                 scierie = new scierie(12, 12, 1);
                 monu = new monument(100, 100, 100);
+                ecole = new ecole(20, 20, 20);
+                entrepos = new entrepos(10, 5, 10);
+                gardeManger = new gardeManger(10, 5, 10);
             }
             public double prodTotalFerme()
             {
@@ -273,6 +327,50 @@ namespace LesColons
                 or = or_prix;
             }
         }
+        class ecole
+        {
+            public double bois;
+            public double or;
+            public double fer;
+            public ecole(double bois_prix, double or_prix, double fer_prix)
+            {
+                fer = fer_prix;
+                bois = bois_prix;
+                or = or_prix;
+            }
+        }
+        class entrepos
+        {
+            public double bois;
+            public double or;
+            public double fer;
+
+            public double stock_fer;
+            public double stock_bois;
+            public entrepos(double bois_prix, double or_prix, double fer_prix)
+            {
+                fer = fer_prix;
+                bois = bois_prix;
+                or = or_prix;
+                stock_bois = 50;
+                stock_fer = 50;
+            }
+        }
+        class gardeManger
+        {
+            public double bois;
+            public double or;
+            public double fer;
+
+            public double stock_nour;
+            public gardeManger(double bois_prix, double or_prix, double fer_prix)
+            {
+                fer = fer_prix;
+                bois = bois_prix;
+                or = or_prix;
+                stock_nour = 50;
+            }
+        }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
@@ -292,11 +390,12 @@ namespace LesColons
         private void timer1_Tick(object sender, EventArgs e)
         {
             plateau.date++;
-            plateau.prestige += 0.05 + 0.5 * plateau.prodTotalMonument();
+            plateau.prestige += 0.05 * plateau.bonheur + 0.5 * plateau.prodTotalMonument();
             plateau.nour -= plateau.pop;
             plateau.nour += plateau.prodTotalFerme();
             plateau.pop += plateau.pop * plateau.nataliter;
             plateau.pop_dispo += plateau.pop * plateau.nataliter;
+            plateau.bonheur += calcul_bonheur(); 
             if (plateau.nour < 0)
             {
                 plateau.pop--;
@@ -306,6 +405,10 @@ namespace LesColons
                 timer1.Stop();
                 MessageBox.Show("perdu");
             }
+            if (plateau.nour > plateau.nour_max) plateau.nour = plateau.nour_max;
+            if (plateau.or > plateau.or_max) plateau.or = plateau.or_max;
+            if (plateau.bois > plateau.bois_max) plateau.bois = plateau.bois_max;
+            if (plateau.fer > plateau.fer_max) plateau.fer = plateau.fer_max;
             //plateau.or += plateau.prodTotalMine();
             plateau.fer += plateau.prodTotalMine();
             plateau.bois += plateau.prodTotalScirie();
@@ -321,6 +424,17 @@ namespace LesColons
             label12.Text = "+" + Convert.ToString(plateau.prodTotalFerme() - plateau.pop);
             label17.Text = "prestige : " + Convert.ToString(plateau.prestige);
             label16.Text = "+" + Convert.ToString(plateau.nataliter * plateau.pop);
+        }
+
+        public double calcul_bonheur()
+        {
+            double indice = 1;
+
+            if (plateau.population_max < plateau.pop) indice -= 1;
+            else indice += 1;
+            if (plateau.pop_dispo >= plateau.pop * 0.5) indice -= 1;
+            else indice += 1;
+            return indice * plateau.pop;
         }
 
         private void pictureBox104_Click(object sender, EventArgs e)
@@ -341,6 +455,26 @@ namespace LesColons
         private void pictureBox107_Click(object sender, EventArgs e)
         {
             select = 6;
+        }
+
+        private void pictureBox108_Click(object sender, EventArgs e)
+        {
+            select = 7;
+        }
+
+        private void pictureBox109_Click(object sender, EventArgs e)
+        {
+            select = 8;
+        }
+
+        private void pictureBox110_Click(object sender, EventArgs e)
+        {
+            select = 9;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) timer1.Enabled = !timer1.Enabled;
         }
     }
 }
